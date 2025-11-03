ab8e79204c3e9346cc3622346c83bd20ff1d438a
INCRED99 <devpathak9801@gmail.com>
2025-11-04 00:21:00 +0530

    Rename parameter in divide function from b to c

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 def multiply(a, b):
     return a * b    
 
-def divide(a, b):
-    if b == 0:
+def divide(a, c):
+    if c == 0:
         raise ValueError("Cannot divide by zero.")
-    return a / b
+    return a / c
